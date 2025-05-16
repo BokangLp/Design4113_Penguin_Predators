7c9aa533dc64a92adf02a88366ac1f97a01f1ced
BokangTheGreat <bokanglepolesa777@gmail.com>
2025-05-16 21:51:42 +0200

    Change image color from RGB to BRG

--- cameraTest.py
+++ cameraTest.py
@@ -14,33 +14,37 @@ GPIO.setup(pin_num, GPIO.IN)
 print("Initializing cam")
 picam2 = Picamera2()
 #picam2.resolution = (2592, 1944)	# Maximize pixels
-picam2.configure(picam2.create_still_configuration(main={"size":(2592,1944)}))
+picam2.configure(picam2.create_still_configuration(main={"size":(1800,1624)}))
 picam2.start()
 print("Cam initialized successfully")
 
 time.sleep(5)
 
 # Update camera settings
-controls = {"AeEnable": False,
-		"ExposureTime": 50000,
-	"AnalogueGain": 5.0
+controls = {"AeEnable": True,
+		#"ExposureTime": 50000,
+	#"AnalogueGain": 5.0
+	"Brightness":0.2,	# -1.0 -> 1.0
+	"Contrast":1.3,		# 0.0 -> 2.0
+	"Saturation":1.0	# 0.0 -> 2.0
 }
 picam2.set_controls(controls)
-#picam2.set_controls({"AwbEnable": True})	# Enable auto white balance
+picam2.set_controls({"AwbEnable": True})	# Enable auto white balance
 
 # Connect to pc
 HOST = '192.168.137.1'
 PORT = 5555
 
-# Create socket
+# Create and connect socket
 #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #sock.connect((HOST,PORT))
 
 # Capture and send image
 def capture_and_send():
-	# Create socket
+	# Create and connect socket
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((HOST,PORT))
+
 	# Capture image
 	try:	
 		print("Capturing image...")
@@ -49,6 +53,8 @@ def capture_and_send():
 	except Exception as e:
 		print(f"Camera capture failed: {e}")
 	
+	# Convert image color toBGR
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 	
 	# Convert image to bytes
 	success, encode_image = cv2.imencode('.jpg',image,[int(cv2.IMWRITE_JPEG_QUALITY),95])
@@ -70,19 +76,19 @@ def capture_and_send():
 num_files = 0
 try:
 	while True:
-		pin_status = GPIO.input(pin_num)
-		if pin_num:
-			print("Motion Sensed! Turning Cam on!") 
-			capture_and_send()
-			num_files+=1
-		else:
-			print("Still safe! No motion")
-			continue
+		#pin_status = GPIO.input(pin_num)
+		#if pin_num:
+		print("Motion Sensed! Turning Cam on!") 
+		capture_and_send()
+		num_files+=1
+		#else:
+		#	print("Still safe! No motion")
+		#	continue
 		time.sleep(5)
 except KeyboardInterrupt:
 	print("Capture loop interrupted")
 finally :
-	sock.close()
+	#sock.close()
 	GPIO.cleanup()
 	print("Connection closed.")
 	print(f"Number of files send : {num_files}. ")
