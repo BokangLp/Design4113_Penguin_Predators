a2019e92fd3a255007a5ce7405d2807dcc40b89f
BokangTheGreat <bokanglepolesa777@gmail.com>
2025-05-27 10:55:22 +0200

    Change from using websockets to using regular sockets

--- Detection_and_Identification/SendDetectionData.py
+++ Detection_and_Identification/SendDetectionData.py
@@ -1,47 +1,58 @@
-import websockets
+import socket
 import base64
 import json
 import asyncio
 import cv2
 import time
-
+import traceback
 
 # Data sending function
-async def send_detection_data(image,timestamp,deterrence):
-	# Link of websocket
-	WS_URI = "ws://192.168.137.1:5557"
-	# Connect to websocket
+async def send_detection_data(image,image_name):
+	# Socket information
+	server_ip = "192.168.137.1"
+	port = 5555
 	try:
-		async with websockets.connect(WS_URI) as websocket:
-			print("Websocket connected")
-	except Exception as e:
-		print("Connection failed")
-		await asyncio.sleep(1)
-	try:
-        # Convert image to bytes
+		# Connect websocket
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((server_ip,port))
+	
+        	# Convert image to bytes
 		success, encode_image = cv2.imencode('.jpg',image,[int(cv2.IMWRITE_JPEG_QUALITY),95])
 		if not success:
 			print("Failed to encode image")
 			return
 		image_bytes = base64.b64encode(encode_image.tobytes()).decode('utf-8')
-		print("Image successfully encode as JPEG")
+		print("Image successfully encoded as JPEG")
 	
 		# Data sent to the server
 		data =  {
-			"timeStamp": time.strftime("%Y-%m-%d %H:%M:%S",timestamp),
-			"image_size": len(encode_image),
+			"image_name": image_name,
 			"image": image_bytes  
 		}
 
 		# Serialize the data
 		json_data = json.dumps(data).encode('utf-8')
-		print("Data serialized successfullyy")			
+		data_size = len(json_data)
+		print("Data serialized successfully")			
 
 		# Send data to the server
-		await websocket.send(json_data)
+		sock.sendall(data_size.to_bytes(4,'big'))
+		sock.sendall(json_data)
 		print("Data sent!")
-		await asyncio.sleep(5)
 	
 	except Exception as e:
 		print(f"Error during sending of data: {e}")
+		traceback.print_exc()
 		await asyncio.sleep(1)
+
+async def main():
+	import DetectAndCapture
+	try:
+		picam2 = DetectAndCapture.initCam()
+		image,image_name = await DetectAndCapture.capture_and_save(picam2)
+		await send_detection_data(image,image_name)
+	except Exception as e:
+		print(f"Exception: {e}")
+
+if __name__ == "__main__":
+	asyncio.run(main())
