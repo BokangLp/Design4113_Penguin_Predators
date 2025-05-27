b165c7ee620e55ce87cdd8fe4f74550b0aea50ee
BokangTheGreat <bokanglepolesa777@gmail.com>
2025-05-27 21:34:55 +0200

    Update the sounds played

--- Detection_and_Identification/DeterrenceCode.py
+++ Detection_and_Identification/DeterrenceCode.py
@@ -3,20 +3,15 @@ import RPi.GPIO as GPIO
 
 # Drive the speakers using specified frequency(Hz) and duty cycle (as a decimal)
 def speakerDrive(frequency,DutyCycle,speaker_Pin):
-	timeON = 1/frequency * DutyCycle	# Calculate time high
-	timeOFF = 1/frequency * (1-DutyCycle)	# Calculate time  Low
-	GPIO.output(speaker_Pin,GPIO.HIGH)	# Set pin High
-	time.sleep(timeON)			# Wait for a duration of timeON
-	GPIO.output(speaker_Pin,GPIO.LOW)	# Set pin low
-	time.sleep(timeOFF)			# wait for ad duration of timeOFF
+	
+	pwm_13 = GPIO.PWM(speaker_Pin,frequency)	# Set GPIO13 to PWM
+	pwm_13.start(DutyCycle)
+	return pwm_13
 
 def ledDrive(frequency,DutyCycle,LED_Pin):
-	timeON = 1/frequency * DutyCycle
-	timeOFF = 1/frequency * (1-DutyCycle)
-	GPIO.output(LED_Pin,GPIO.HIGH)
-	time.sleep(timeON)
-	GPIO.output(LED_Pin, GPIO.LOW)
-	time.sleep(timeOFF)
+	pwm_12 = GPIO.PWM(LED_Pin,frequency)
+	pwm_12.start(DutyCycle)
+	return pwm_12
 
 def motorDrive(motor_pin):
 	GPIO.output(motor_pin,GPIO.HIGH)
